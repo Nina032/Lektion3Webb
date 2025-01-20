5250cfe62054be730ca4f40f2a0011ce84483216
Nevena Kicanovic <nevena@contactus.se>
2025-01-20 16:28:03 +0100

    Add project files.

---
+++ Lektion3Web/Controllers/LocationsController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lektion3Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Lektion3Web.Controllers
+{
+    public class LocationsController : Controller
+    {
+        private readonly static string COUNTRIES_KEY = "Globocations";
+
+        private IMemoryCache countryCache;
+        private CosmosClient docClient;
+        private Microsoft.Azure.Cosmos.Container docContainer;
+        public LocationsController(IMemoryCache cache, CosmosClient client)
+        {
+            countryCache = cache;
+            docClient = client;
+            docContainer = docClient.GetContainer("lektion3cdb", "lektion3locations");
+        }
+        public async Task<IActionResult> Index()
+        {
+            //get countries for search list
+            var countries = await GetCountriesListAsync();
+            ViewData["CountriesList"] = countries;
+
+            //get the list of all locations from CosmosDb
+            var locations = new List<LocationModel>();
+
+            var iterator = docContainer.GetItemQueryIterator<LocationModel>();
+            while (iterator.HasMoreResults)
+            {
+                var pageOfLocations = await iterator.ReadNextAsync();
+                locations.AddRange(pageOfLocations.ToList());
+            }
+
+            //pass locations and countries
+            return View(locations);
+        }
+
+        private async Task<List<string>> GetCountriesListAsync()
+        {
+            var countries = await countryCache.GetOrCreateAsync<List<string>>(
+                COUNTRIES_KEY, (entry) => {
+                    //TODO: Get countries from CosmosDB
+                    
+                    return Task.FromResult(new List<string>(new string[]{ "United States" }));
+                });
+
+            return countries;
+           
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(LocationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //Add a new item to CosmosDB
+                model.Id = Guid.NewGuid().ToString();
+                await docContainer.CreateItemAsync(
+                    model, new PartitionKey(model.Country)
+                    );
+
+                return RedirectToAction("index");
+            }
+            else
+            {
+                return View();
+            }
+
+            
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Search([FromQuery] string country)
+        {
+            //get all locations for a country
+            var iterator = docContainer.GetItemQueryIterator<LocationModel>(
+                requestOptions: new QueryRequestOptions()
+                {
+                    PartitionKey = new PartitionKey(country)
+                });
+            var locations = new List<LocationModel>();
+            return View(locations);
+        }
+
+    }
+}
---
+++ Lektion3Web/Models/Lektion3DBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lektion3Web.Models
+{
+    public class Lektion3DBContext : DbContext
+    {
+        public Lektion3DBContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public virtual DbSet<BrandModel> Brands { get; set; }
+
+    }
+}
